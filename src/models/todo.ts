c2e8792c1c4b9568a1a8f96d5837146f1a9d6595
Danischolar <olayinkaoluwaseun90@gmail.com>
2023-07-19 12:20:17 +0100

    new

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,2 +1,8 @@
-import { ITodo } from "./../types/todo"
-import { model, Schema } from "mongoose"
+import { ITodo } from "./../types/todo";
+import { model, Schema } from "mongoose";
+
+const todoSchema: Schema = new Schema({
+  name: {
+    type: String,
+  },
+});
