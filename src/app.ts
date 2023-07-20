e746f4effa9354ed2f96d5b28e060d0eafe41668
Danischolar <olayinkaoluwaseun90@gmail.com>
2023-07-20 02:42:35 +0100

    2:42AM

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,21 @@ import mongoose from 'mongoose'
 import cors from "cors"
 import todoRoutes from "./routes"
 
-const app:
+const app: Express = express();
+
+const PORT: string | number = process.env.PORT || 4000
+
+app.use(cors())
+app.use(todoRoutes())
+
+const uri: string = `mongodb+srv://Danischolar:olayinka@cluster0.q3ad4ae.mongodb.net/?retryWrites=true&w=majority`
+
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.set("useFindAndModify", false)
+
+mongoose.connect(uri, options)
+.then(() => 
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+).catch (error => {
+    throw error
+})
