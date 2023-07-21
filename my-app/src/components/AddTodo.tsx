643721e26e86caddd3821702a2fb9e247c3c212a
Danischolar <olayinkaoluwaseun90@gmail.com>
2023-07-22 00:38:37 +0100

    Add Todo Components

--- my-app/src/components/AddTodo.tsx
+++ my-app/src/components/AddTodo.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
 type Props = TodoProps & {
-updateTodo: (todo: ITodo) => void
-deleteTodo: (todo: ITodo) => void
+    updateTodo: (todo: ITodo) => void
+    deleteTodo: (_id: string) => void
+}
+
+const Todo: React.FC<Props> = ({
+    todo,
+    updateTodo,
+    deleteTodo
+}) => {
+    const checkTodo: string = todo.status ? `line-through` : " "
+    return (
+        <div>
+        </div>
+    )
 }
