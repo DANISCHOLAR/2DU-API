037b9cd1c3a01f4c87d1a82bbded0d2ec0aa019a
Danischolar <olayinkaoluwaseun90@gmail.com>
2023-07-21 02:51:02 +0100

    working on Clientside React

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const PORT: string | number = process.env.PORT || 4000
 app.use(cors())
 app.use(todoRoutes)
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${prooces.env.MONGO_PASSWORD}@cluster0.q3ad4ae.mongodb.net/?retryWrites=true&w=majority`
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.q3ad4ae.mongodb.net/?retryWrites=true&w=majority`
 
 // const options = { useNewUrlParser: true, useUnifiedTopology: true }
 // mongoose.set("useFindAndModify", false)
