34c469c53ea92bb20d5c6cd3668675c1417ee528
Danischolar <olayinkaoluwaseun90@gmail.com>
2023-07-24 22:41:13 +0100

    Fixing Components

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -18,7 +18,7 @@ const App: React.FC = () => {
   }
 
   const handleSaveTodo = (e: React.FormEvent, formData: ITodo): void => {
-    e.preventDefault();
+    e.preventDefault()
     addTodo(formData)
       .then(({ status, data }) => {
         if (status !== 201) {
@@ -38,20 +38,35 @@ const App: React.FC = () => {
         setTodos(data.todos)
       })
       .catch(err => console.log(err))
-    }
+  }
 
 
-    const handleDeleteTodo = (_id: string): void => {
-      deleteTodo(_id)
-      .then(({status, data}) => {
+  const handleDeleteTodo = (_id: string): void => {
+    deleteTodo(_id)
+      .then(({ status, data }) => {
         if (status !== 200) {
           throw new Error("Error! Todo not deleted")
         }
         setTodos(data.todos)
       })
       .catch(err => console.log(err))
-    }
+  }
 
+
+  return (
+    <main className='App'>
+      <h1> My Todos</h1>
+      <AddTodo saveTodo={handleSaveTodo} />
+      {todos.map((todo: ITodo) => (
+        <TodoItem 
+        key={todo._id} 
+        todo={todo}
+         updateTodo={handleUpdateTodo}
+          deleteTodo={handleDeleteTodo} />
+      ))}
+
+    </main>
+  )
 
 }
 
